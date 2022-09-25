c3d2fe1566588ff41d0a143c0ddc36f0d60c6afe
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-09-25 00:45:43 -0400

    fix tilt effect with new package

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,37 +1,46 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { Box } from '@chakra-ui/layout'
-import { useColorMode } from '@chakra-ui/color-mode'
-import Tilt from './Tilt'
+import React from "react";
+import { motion } from "framer-motion";
+import { Box } from "@chakra-ui/layout";
+import { useColorMode } from "@chakra-ui/color-mode";
 
 interface CardProps {
-	children: React.ReactNode,
-	delay?: number,
-	tilt?: boolean,
-	duration?: number,
-	noShadow?: boolean,
-	padding?: number,
-	mt?: number,
-	mb?: number,
+	children: React.ReactNode;
+	delay?: number;
+	duration?: number;
+	noShadow?: boolean;
+	padding?: number;
+	mt?: number;
+	mb?: number;
 }
 
 export default function Card(props: CardProps) {
 	const { colorMode } = useColorMode();
-	const shadow = `${!props.noShadow ? `5px 5px 25px 10px ${colorMode === "dark" ? "rgba(255, 255, 255, 0.25)" : "rgba(0, 0, 0, 0.25)"} , -5px -5px 25px 10px ${colorMode === "dark" ? "rgba(255, 255, 255, 0.22" : "rgba(0, 0, 0, 0.22)"}` : "none"}`;
+
+	const shadow = `${
+		!props.noShadow
+			? `5px 5px 25px 10px ${
+					colorMode === "dark"
+						? "rgba(255, 255, 255, 0.25)"
+						: "rgba(0, 0, 0, 0.25)"
+			  } , -5px -5px 25px 10px ${
+					colorMode === "dark"
+						? "rgba(255, 255, 255, 0.22"
+						: "rgba(0, 0, 0, 0.22)"
+			  }`
+			: "none"
+	}`;
 
 	return (
-		<Tilt tilt={props.tilt}>
-			<motion.div
-				initial={{ opacity: 0, scale: 0.95 }}
-				animate={{ opacity: 1, scale: 1, boxShadow: shadow}}
-				transition={{ duration: props.duration || 0.75, delay: props.delay || 0 }}
-				className={`main-inner-box-${colorMode}`}
-				style={{'marginTop': props.mt || 0, 'marginBottom': props.mb || 0}}
-			>
-				<Box p={props.padding || 10} transform='translateZ(100px)'>
-					{props.children}
-				</Box>
-			</motion.div>
-		</Tilt>
-	)
+		<motion.div
+			initial={{ opacity: 0, scale: 0.95 }}
+			animate={{ opacity: 1, scale: 1, boxShadow: shadow }}
+			transition={{ duration: props.duration || 0.75, delay: props.delay || 0 }}
+			className={`main-inner-box-${colorMode}`}
+			style={{ marginTop: props.mt || 0, marginBottom: props.mb || 0 }}
+		>
+			<Box p={props.padding || 10} transform="translateZ(100px)">
+				{props.children}
+			</Box>
+		</motion.div>
+	);
 }
--- pages/creations.tsx
+++ pages/creations.tsx
@@ -1,46 +1,40 @@
-import React from 'react'
-import {
-	Heading,
-	Box,
-	Stack,
-	SimpleGrid,
-	Text,
-} from '@chakra-ui/layout';
-import { Image } from '@chakra-ui/react';
-import Typewriter from 'typewriter-effect';
-import { useColorMode } from '@chakra-ui/color-mode';
-import Card from '../components/Card';
-import Tilt from '../components/Tilt';
-import { motion } from 'framer-motion';
+import React from "react";
+import { Heading, Box, Stack, SimpleGrid, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+import Typewriter from "typewriter-effect";
+import { useColorMode } from "@chakra-ui/color-mode";
+import Card from "../components/Card";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
 
 const creations = [
 	{
 		name: "QR Tools",
 		link: "https://qr.tinu.tech",
-		img: 'https://dl.airtable.com/.attachments/53c69098ee24adfb06cd4d4ba694072b/9046f2c2/Screenshot2021-11-02213547.png'
+		img: "https://dl.airtable.com/.attachments/53c69098ee24adfb06cd4d4ba694072b/9046f2c2/Screenshot2021-11-02213547.png",
 	},
 	{
 		name: "Pollster",
 		link: "https://pollster.tinu.tech",
-		img: 'https://dl.airtable.com/.attachments/2aa9d3261280c735dbc81e14a4944a1a/61020969/pollster.png'
+		img: "https://dl.airtable.com/.attachments/2aa9d3261280c735dbc81e14a4944a1a/61020969/pollster.png",
 	},
 	{
 		name: "Digital Fit",
 		link: "https://fit.tinu.tech",
-		img: "https://dl.airtable.com/.attachments/d506ac2f66ce77f2ee137f4fb8f8f5eb/3efa6682/digitalFit.png"
+		img: "https://dl.airtable.com/.attachments/d506ac2f66ce77f2ee137f4fb8f8f5eb/3efa6682/digitalFit.png",
 	},
 	{
 		name: "Homework Manager",
 		link: "https://homework.tinu.tech",
-		img: "https://dl.airtable.com/.attachments/a7341132e86d622498ca2becdca3516e/84da3885/homeworkManager.jpg"
-	}
-]
+		img: "https://dl.airtable.com/.attachments/a7341132e86d622498ca2becdca3516e/84da3885/homeworkManager.jpg",
+	},
+];
 
 export default function Creations() {
 	const { colorMode } = useColorMode();
 
 	return (
-		<Box className = "main-outer-box">
+		<Box className="main-outer-box">
 			<Heading textAlign="center">
 				<Typewriter
 					onInit={(typewriter) => {
@@ -48,61 +42,76 @@ export default function Creations() {
 					}}
 				/>
 			</Heading>
-			<SimpleGrid columns = {[1, 2, 3]} spacing = {7} mt={10} textAlign="center">
+			<SimpleGrid columns={[1, 2, 3]} spacing={7} mt={10} textAlign="center">
 				{creations.map((creation, i) => (
-					<Card padding={6} noShadow delay = {0.3 * i} key = {i} duration = {0.5} mt={12} mb={10}>
-						<Box>
+					<Card
+						padding={6}
+						noShadow
+						delay={0.3 * i}
+						key={i}
+						duration={0.5}
+						mt={12}
+						mb={10}
+					>
+						<Box mt={-12}>
 							<motion.div
-								initial={{ 
+								initial={{
 									opacity: 0,
-									y: 25
+									y: 25,
 								}}
 								animate={{
 									opacity: 1,
-									y: 0
+									y: 0,
 								}}
 								transition={{
 									duration: 0.5,
-									delay: 0.3 + 0.3 * i
+									delay: 0.3 + 0.3 * i,
 								}}
 							>
-								<Tilt tilt>
+								<Tilt
+									tiltMaxAngleX={10}
+									tiltMaxAngleY={10}
+									transitionSpeed={2000}
+									scale={1.05}
+									glareEnable
+									glareBorderRadius="10px"
+									glareMaxOpacity={0.75}
+								>
 									<Box
-										rounded={'lg'}
-										mt={-12}
-										pos={'relative'}
-										height={'150px'}
-										//overflow={'hidden'}
+										rounded={"lg"}
+										pos={"relative"}
+										height={"150px"}
 										_after={{
-											transition: 'all .3s ease',
+											transition: "all .3s ease",
 											content: '""',
-											w: 'full',
-											h: 'full',
-											pos: 'absolute',
+											w: "full",
+											h: "full",
+											pos: "absolute",
 											top: 0,
 											left: 0,
 											backgroundImage: creation.img,
-											backgroundSize: 'cover',
-											filter: 'blur(20px)',
+											backgroundSize: "cover",
+											backgroundPosition: "center",
+											filter: "blur(20px)",
 											zIndex: -1,
 										}}
-										>
+									>
+										<a target="blank" href={creation.link}>
 											<Image
-												rounded={'lg'}
+												rounded={"lg"}
 												height={150}
-												objectFit={'cover'}
+												objectFit={"cover"}
 												src={creation.img}
 											/>
+										</a>
 									</Box>
 								</Tilt>
 							</motion.div>
 						</Box>
-						<Text py="5">
-							{creation.name}
-						</Text>
+						<Text py="5">{creation.name}</Text>
 					</Card>
 				))}
 			</SimpleGrid>
 		</Box>
-	)
+	);
 }
